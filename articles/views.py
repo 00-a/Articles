cd9b8a52ace6d482c2cd697260280dd28f0021dd
ezz <ezz@MacBook-Air-Inna.local>
2021-05-07 13:58:57 +0500

    Add base.html and main_page.html

---
+++ articles/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from apps.articles.services import get_articles, get_context_from_articles
+
+
+class MainPageView(TemplateView):
+    """Main page. All articles on the site"""
+    template_name = 'articles/main_page.html'
+
+    def get(self, request):
+        articles = get_articles()
+        return render(request, self.template_name, get_context_from_articles(articles))
+
