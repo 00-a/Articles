fbc32c6ca26d39cf561fc334403ebed663e9f1db
ezz <ezz@MacBook-Air-Inna.local>
2021-05-07 13:31:17 +0500

    Create models

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    """Article"""
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    change_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("articleDetail", kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+
+class Tag(models.Model):
+    """Tag for article"""
+    name = models.CharField(max_length=50, verbose_name="Tag name")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
