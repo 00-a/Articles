84a0b183d0ca06a511c3a629f5df1bddfd77a040
00-a <00-a@ex.com>
2021-05-08 09:09:43 +0500

    Code refactoring

--- articles/services.py
+++ articles/services.py
@@ -1,11 +1,10 @@
-from django.db.models import QuerySet
 from articles.models import Article
 
 
-def get_articles(**kwargs) -> QuerySet:
+def get_articles(**kwargs) -> 'QuerySet':
     return Article.objects.filter(**kwargs)
 
 
-def get_context_from_articles(articles: QuerySet) -> dict:
+def get_context_from_articles(articles: 'QuerySet') -> dict:
     """Get article(s) if it exists, else get message"""
     return {'articles': articles} if articles else {'message': 'Article(s) does not exist(s)'}
