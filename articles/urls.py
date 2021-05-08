2569ba96a1a0e62d6857f8100544aaa41e6387bd
00-a <00-a@ex.com>
2021-05-08 17:16:06 +0500

    Code refactoring

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import MainPageView, ArticleDetailView, UserRegistrationView
 
+
 urlpatterns = [
     path('', MainPageView.as_view(), name='mainPage'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='articleDetail'),
