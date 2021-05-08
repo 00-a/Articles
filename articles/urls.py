215328b952efde9e2a7131c396cd71fe9a406fdd
00-a <qwertqwerty70@gmail.com>
2021-05-08 17:20:39 +0500

    Code refactoring

--- articles/urls.py
+++ articles/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from .views import MainPageView, ArticleDetailView, UserRegistrationView
 
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='mainPage'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='articleDetail'),
-    path('accounts/registration/', UserRegistrationView.as_view(), name='registration'),
+    path('accounts/registration/§', UserRegistrationView.as_view(), name='registration'),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout')
 ]
