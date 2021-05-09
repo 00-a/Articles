d3346b7280979035cccf88116be474cafa2b9184
00-a <qwertqwerty70@gmail.com>
2021-05-09 09:14:21 +0500

    Create all_articles_by_user.html

--- articles/urls.py
+++ articles/urls.py
@@ -7,7 +7,7 @@ from .views import MainPageView, ArticleDetailView, UserRegistrationView
 urlpatterns = [
     path('', MainPageView.as_view(), name='mainPage'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='articleDetail'),
-    path('accounts/registration/§', UserRegistrationView.as_view(), name='registration'),
+    path('accounts/registration/', UserRegistrationView.as_view(), name='registration'),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout')
 ]
